2e7ef89c4fd168e67d2131fe1baaa9612a4ac92e
Stefan Agner <stefan@agner.ch>
2018-08-14 16:46:16 +0200

    block: weaken warning on block writes
    partprobe seems to trigger a write on the eMMC boot partition
    as well. Print only a warning message instead of a full stack
    trace.
    
    Signed-off-by: Stefan Agner <stefan@agner.ch>

--- block/blk-core.c
+++ block/blk-core.c
@@ -2155,7 +2155,7 @@ static inline bool bio_check_ro(struct bio *bio, struct hd_struct *part)
 	if (part->policy && op_is_write(bio_op(bio))) {
 		char b[BDEVNAME_SIZE];
 
-		WARN_ONCE(1,
+		printk(KERN_WARNING
 		       "generic_make_request: Trying to write "
 			"to read-only block-device %s (partno %d)\n",
 			bio_devname(bio, b), part->partno);
